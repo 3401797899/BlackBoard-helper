b6b94546b28a0e6fe3a1fe6a235fc9fafb0167f1
安小白 <3401797899@qq.com>
2023-08-21 01:40:57 +0800

    feat: 适应新版信息门户

--- utils/login.py
+++ utils/login.py
@@ -39,49 +39,50 @@ class BBLogin:
             return rsa_text.decode('utf-8')
 
     def check_password(self, username, password):
-        data = {
-            'username': username,
-            'password': self.encrypt(password)
-        }
+        url = 'https://id-ouc-edu-cn-s.otrust.ouc.edu.cn/baseLogin/postLogin?sf_request_type=ajax'
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39',
         }
-        t = requests.post('https://id-ouc-edu-cn-8071-p.otrust.ouc.edu.cn/sso/ssoLogin?sf_request_type=ajax', data=data,
-                          headers=headers, proxies=proxies, verify=False)
-        if not t.text == '{"state":true}':
+        response = requests.post(url, headers=headers, verify=False, json={
+            'account': username,
+            'password': self.encrypt(password),
+            'randomCode': 'comsys-base-login',
+            'isRememberPwdOpen': 0,
+            'scale': 1
+        }, proxies=proxies).json()
+        # 获取姓名
+        # name = response['datas']['casUser']['personName']
+        if not response['code'] == 500:
             return False
         return True
 
     def get_session_id(self, username, password):
         r = requests.session()
+        url = 'https://id-ouc-edu-cn-s.otrust.ouc.edu.cn/sso/login?service=https://wlkc.ouc.edu.cn/webapps/bb-sso-BBLEARN/index.jsp#/'
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39',
         }
-        r.get(
-            'https://id-ouc-edu-cn-8071-p.otrust.ouc.edu.cn/sso/login?service=https%3A%2F%2Fwlkc.ouc.edu.cn%2Fwebapps%2Fbb-sso-BBLEARN%2Findex.jsp',
-            verify=False, headers=headers, proxies=proxies)
-        data = {
+        content = r.get(url, headers=headers, verify=False, proxies=proxies).text
+        geolocation = re.search('<input type="hidden" name="execution" value="(.*?)"', content)
+
+        self.check_password(username, password)
+
+        url = 'https://id-ouc-edu-cn-s.otrust.ouc.edu.cn/sso/login'
+        t = r.post(url, headers=headers, data={
+            'execution': geolocation.group(1),
+            '_eventId': 'submit',
+            'geolocation': '',
             'username': username,
-            'password': self.encrypt(password)
-        }
-        t = r.post('https://id-ouc-edu-cn-8071-p.otrust.ouc.edu.cn/sso/ssoLogin', data=data, headers=headers,
-                   proxies=proxies, verify=False)
-        if not t.text == '{"state":true}':
-            return None
-        url = 'https://id-ouc-edu-cn-8071-p.otrust.ouc.edu.cn/sso/login?service=http://id-ouc-edu-cn-8071-p.otrust.ouc.edu.cn/j_spring_cas_security_check;jsessionid=' + r.cookies.get(
-            'JSESSIONID', domain='id-ouc-edu-cn-8071-p.otrust.ouc.edu.cn', path='/sso/')
-        data = {
-            'username': username,
-            'password': str(base64.b64encode(password.encode('utf-8')).decode('utf-8')),
-            'lt': 'e1s1',
-            '_eventId': 'submit'
-        }
-        r.post(url=url, data=data, headers=headers, proxies=proxies, verify=False)
-        url = "https://wlkc.ouc.edu.cn/webapps/bb-sso-BBLEARN/index.jsp"
-        t = r.get(url=url, headers=headers, proxies=proxies, verify=False)
-        token = re.findall('<input type="hidden" value="(.*?)" name="token"/>', t.text)[0]
+            'password': '11011100',  # 随机？
+            'service': 'https://wlkc.ouc.edu.cn/webapps/bb-sso-BBLEARN/index.jsp#/'
+        }, verify=False, proxies=proxies)
+
+        content = r.get(url='https://wlkc.ouc.edu.cn/webapps/bb-sso-BBLEARN/index.jsp', headers=headers, verify=False,
+                        proxies=proxies).text
+        token = re.search('<input type="hidden" value="(.*?)" name="token"/>', content).group(1)
+
         url = 'https://wlkc.ouc.edu.cn/webapps/bb-sso-BBLEARN/execute/authValidate/customLogin'
-        r.post(url=url, data={'username': username, 'token': token}, headers=headers, proxies=proxies, verify=False)
+        r.post(url=url, data={'username': username, 'token': token}, verify=False, proxies=proxies)
         s_session_id = r.cookies.get('s_session_id', domain='wlkc.ouc.edu.cn', path='/')
         return f's_session_id={s_session_id};'
 
